5826196aa9778a0594fd15e991d3dcd689840a33
amazingwww <60274233+amazingwww@users.noreply.github.com>
2026-06-14 02:13:53 +0100

    Update app.ts

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,8 +1,13 @@
 import express from "express";
 const pinoHttp = require("pino-http");
+import { logger } from "./lib/logger";
 
 const app = express();
 
-app.use(pinoHttp());
+app.use(
+  pinoHttp({
+    logger,
+  })
+);
 
 export default app;
